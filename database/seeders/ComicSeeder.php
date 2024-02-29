3616d8c2d1e76ea2bcd6d114e997a1b3ff294d90
CappelliLeonardo <l.cappelli1995@gmail.com>
2024-02-29 15:47:32 +0100

    Milestone 1 & bonus completed

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+// Models 
+use App\Models\Admin\Comic;
 class ComicSeeder extends Seeder
 {
     /**
@@ -12,6 +14,28 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //prendo dati dal file comics.php nel config folder
+        $comicsData = config('comics');
+
+        foreach ($comicsData as $index => $singleComic) {
+             
+            $comic = new comic();
+            $comic->title = $singleComic['title'];
+            $comic->description = $singleComic['description'];
+            $comic->thumb = $singleComic['thumb'];
+            
+            $explodePrice = explode('$', $singleComic['price']);
+            $comic->price = floatval($explodePrice[1]);
+
+            $comic->series = $singleComic['series'];
+            $comic->sale_date = $singleComic['sale_date'];
+            $comic->type = $singleComic['type'];
+
+            $comic->artists = json_encode($singleComic['artists']);
+            $comic->writers = json_encode($singleComic['writers']);
+
+            $comic->save();
+        };
+        
     }
 }
